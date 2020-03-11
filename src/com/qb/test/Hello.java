9226dd1856a22e66dc7c4f9870ea443145ba3638
zoulinjun <zoulinjun@fortunebill.com>
2020-03-11 13:49:50 +0800

    frist commit

---
+++ src/com/qb/test/Hello.java
@@ -0,0 +1,13 @@
+package com.qb.test;
+
+/**
+ * 功能：
+ *
+ * @author zoulinjun
+ * @date 2020/3/11
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
